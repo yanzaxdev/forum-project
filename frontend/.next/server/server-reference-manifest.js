17786792a1409367c3b62e6f0a293e41a02f52f0
yaniv <yanivzaxdev@gmail.com>
2024-12-24 18:44:29 +0200

    Remove .next

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WQVR4AZh9xOUHJNX8vSTVh8DXU/psMvkBfiiLa/fhRw=\"\n}"
--- frontend/.next/transform.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "transform.js";
-const runtime = require("./build/chunks/[turbopack]_runtime.js");
-runtime.loadChunk("build/chunks/[root of the server]__2d4c17._.js");
-runtime.loadChunk("build/chunks/postcss_config_js_transform_ts_64f1ae._.js");
-runtime.getOrInstantiateRuntimeModule("[turbopack-node]/globals.ts [postcss] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[project]/frontend/postcss.config.js/transform.ts { CONFIG => \\\"[project]/frontend/postcss.config.js_.loader.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
